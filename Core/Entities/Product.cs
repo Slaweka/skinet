ee8bc5e8ad4a6297b1b222f79079a0763d6335a2
Sławomir Adrych <slawomir.adrych@gmail.com>
2020-08-31 16:30:27 +0200

    Pierwszy commit

---
+++ Core/Entities/Product.cs
@@ -0,0 +1,10 @@
+namespace Core.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        //Test
+        public string NameTest { get; set; }
+    }
+}
